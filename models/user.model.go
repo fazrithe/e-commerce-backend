175e6e0f62afe70dc2218c926c099bf6bebb5617
MOH DEDE FAZRI <fazrithe@gmail.com>
2023-02-08 14:49:26 +0700

    first commit

---
+++ models/user.model.go
@@ -0,0 +1,53 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	// ID        uint   `gorm:"primary_key"`
+	Name             string `gorm:"type:varchar(255);not null"`
+	Email            string `gorm:"uniqueIndex;not null"`
+	Password         string `gorm:"not null"`
+	Role             string `gorm:"type:varchar(255);not null"`
+	Provider         string `gorm:"not null"`
+	Photo            string `gorm:"null"`
+	PhotoName        string `gorm:"type:varchar(255);null"`
+	PhotoPath        string `gorm:"type:varchar(255);null"`
+	VerificationCode string
+	Verified         bool `gorm:"not null"`
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+}
+
+type SignUpInput struct {
+	Name            string `json:"name" binding:"required"`
+	Email           string `json:"email" binding:"required"`
+	Password        string `json:"password" binding:"required,min=8"`
+	PasswordConfirm string `json:"passwordConfirm" binding:"required"`
+	Photo           string `json:"photo"`
+	PhotoName       string `json:"photo_name"`
+	PhotoPath       string `json:"photo_path"`
+	Role            string `json:"role"`
+}
+
+type SignInInput struct {
+	Email    string `json:"email"  binding:"required"`
+	Password string `json:"password"  binding:"required"`
+}
+
+type UserResponse struct {
+	ID        uuid.UUID `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Role      string    `json:"role,omitempty"`
+	Photo     string    `json:"photo"`
+	PhotoName string    `json:"photo_name"`
+	PhotoPath string    `json:"photo_path"`
+	Provider  string    `json:"provider"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
