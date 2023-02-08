570eb339cea9ab2702fabc6b9ef09ab915ab4246
MOH DEDE FAZRI <fazrithe@gmail.com>
2023-02-08 15:08:13 +0700

    test connect

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/fazrithe/e-commerce-backend/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	server *gin.Engine
+)
+
+func init() {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatal("? Could not load environment variables", err)
+	}
+
+	initializers.ConnectDB(&config)
+
+	server = gin.Default()
+}
+
+func main() {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatal("? Could not load environment variables", err)
+	}
+
+	router := server.Group("/api")
+	router.GET("/healthchecker", func(ctx *gin.Context) {
+		message := "Welcome to golang with Gorm and Prostgres"
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": message})
+	})
+
+	log.Fatal(server.Run(":" + config.ServerPort))
+}
